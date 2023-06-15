6225ff4c2651ddf2dcce9024db95f9f9c83a94e6
Sanjay Chandra Patil <121090789+sanjay7008@users.noreply.github.com>
2023-06-15 20:38:32 +0530

    Update task3-2.py

--- task3-2.py
+++ task3-2.py
@@ -3,13 +3,14 @@ def compute(n):
         out = n ** 2
     elif n < 20:
         out = 1
-        for i in range(1, n-9): #2
+        for i in range(1, n-9): #changed -10 to -9 as range runs upto n-1 iterations
             out *= i
     else:
         lim = n - 20
         out = lim * lim
-        out = out + lim #1
+        out = out + lim #changed - operator to + operator
         out = out / 2 
     print(out)
     
-compute(8)
+n = int(input("Enter an integer: "))
+compute(n)
